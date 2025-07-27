56cda0d682a7439181ef6252a16abfce2f1d56d3
pasindu2001-SW <pasindubhasurabala@gmail.com>
2025-07-28 01:15:43 +0530

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
+};
+
+export default config;
