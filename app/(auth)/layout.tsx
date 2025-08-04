f4acf3f9ffe5a3e49532f7e7889096eeb9aec73f
pasindu2001-SW <pasindubhasurabala@gmail.com>
2025-08-04 19:54:44 +0530

    fixed redirect loop

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ const AuthLayout = async ({children}: {children: ReactNode}) => {
 
    const isUserAuthenticated = await isAuthenticated();
   
-    if (!isUserAuthenticated) redirect('/');
+    if (isUserAuthenticated) redirect('/');
   
   return (
     <div className="auth-layout">{children}</div>
